f0161029e82493c939380f8b9af54ea07d745397
Dylan Park <dylan.park@att.net>
2022-11-24 01:19:29 -0600

    Implement the basic functionality direct from TwitterFxAdder, the functionality will be altered and extended from here

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
 import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.io.IOException;
+import java.util.regex.Pattern;
 
 public class Main {
     static boolean disabled = false;
+    static int retryCount = 0;
     // load an image
     static Image defaultImage = Toolkit.getDefaultToolkit().getImage(Main.class.getResource("default.png"));
     static Image disabledImage = Toolkit.getDefaultToolkit().getImage(Main.class.getResource("disabled.png"));
@@ -78,5 +85,39 @@ public class Main {
 //            trayIcon.setImage(updatedImage);
 //        }
 // ...
+
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+
+        clipboard.addFlavorListener(e -> {
+            if (!disabled) {
+                try {
+                    processClipboard(clipboard);
+                } catch (IllegalStateException ex) {
+                    if (retryCount <= 5) {
+                        try {
+                            retryCount++;
+                            Thread.sleep(50);
+                            processClipboard(clipboard);
+                        } catch (IOException | UnsupportedFlavorException | InterruptedException exc) {
+                            throw new RuntimeException(exc);
+                        }
+                    } else {
+                        retryCount = 0;
+                        System.out.println("Clipboard Inaccessible");
+                        throw new RuntimeException(ex);
+                    }
+                } catch (IOException | UnsupportedFlavorException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+    }
+
+    public static void processClipboard(Clipboard clipboard) throws IOException, UnsupportedFlavorException {
+        String input = clipboard.getData(DataFlavor.stringFlavor).toString();
+        if (Pattern.compile("http(?:s)?:\\/\\/(?:www)?twitter\\.com\\/([a-zA-Z0-9_]+)\\/status(\\/.*)?").matcher(input).matches()) {
+            StringSelection selection = new StringSelection(input.substring(0, input.indexOf("//") + 2) + "fx" + input.substring(input.indexOf("twitter")));
+            clipboard.setContents(selection, selection);
+        }
     }
 }
