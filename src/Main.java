e66ae542292ae4017135ec58b18bc4c946dc90b8
Dylan Park <dylan.park@att.net>
2022-11-28 15:21:47 -0600

    Potentially more stable clipboard handling (Less dropouts, text only), https://stackoverflow.com/a/14226456/8630583

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,7 @@ import org.json.JSONObject;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.StringSelection;
-import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.datatransfer.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemListener;
 import java.io.File;
@@ -18,7 +15,7 @@ import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Main {
+public class Main extends Thread implements ClipboardOwner {
     static String folderPath = System.getenv().get("APPDATA") + "\\ClipboardBuddy";
     static String filePath = folderPath + "\\rules.json";
     static Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
@@ -27,11 +24,11 @@ public class Main {
     static boolean disabled = false;
     static boolean historyOpen = false;
     static boolean optionsOpen = false;
-    static int retryCount = 0;
     // load an image
     static Image defaultImage = Toolkit.getDefaultToolkit().getImage(Main.class.getResource("default.png"));
     static Image disabledImage = Toolkit.getDefaultToolkit().getImage(Main.class.getResource("disabled.png"));
     static java.util.List<Image> defaultImageIcons = new ArrayList<>();
+    Clipboard sysClip = Toolkit.getDefaultToolkit().getSystemClipboard();
 
     public static void main(String[] args) throws IOException {
         defaultImageIcons.add(Toolkit.getDefaultToolkit().getImage(Main.class.getResource("default.png")));
@@ -41,6 +38,10 @@ public class Main {
         createDataFile();
         loadDataFile();
         saveDataFile();
+
+        Main mainListener = new Main();
+        mainListener.start();
+
         // get the SystemTray instance
         SystemTray tray = SystemTray.getSystemTray();
         TrayIcon trayIcon = new TrayIcon(defaultImage, "ClipboardBuddy");
@@ -80,7 +81,7 @@ public class Main {
                 historyList.add(s);
             }
             frame.getContentPane().add(historyList, BorderLayout.CENTER);
-            frame.setBounds(((int)size.getWidth()/2)-250, ((int)size.getHeight()/2)-250, 500, 500);
+            frame.setBounds(((int) size.getWidth() / 2) - 250, ((int) size.getHeight() / 2) - 250, 500, 500);
             frame.setVisible(true);
         };
 
@@ -88,7 +89,7 @@ public class Main {
             JFrame frame = new JFrame("Options");
             frame.setIconImages(defaultImageIcons);
 //            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setBounds(((int)size.getWidth()/2)-250, ((int)size.getHeight()/2)-250, 500, 500);
+            frame.setBounds(((int) size.getWidth() / 2) - 250, ((int) size.getHeight() / 2) - 250, 500, 500);
             frame.setVisible(true);
         };
 
@@ -123,66 +124,64 @@ public class Main {
         } catch (AWTException e) {
             System.err.println(e);
         }
-        // ...
 
-// ...
-// some time later
-// the application state has changed - update the image
-//        if (trayIcon != null) {
-//            trayIcon.setImage(updatedImage);
-//        }
-// ...
-
-        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-
-        clipboard.addFlavorListener(e -> {
-            if (!disabled) {
-                try {
-                    processClipboard(clipboard);
-                } catch (IllegalStateException ex) {
-                    if (retryCount <= 5) {
-                        try {
-                            retryCount++;
-                            Thread.sleep(50);
-                            processClipboard(clipboard);
-                        } catch (IOException | UnsupportedFlavorException | InterruptedException exc) {
-                            throw new RuntimeException(exc);
-                        }
-                    } else {
-                        retryCount = 0;
-                        System.out.println("Clipboard Inaccessible");
-                        throw new RuntimeException(ex);
-                    }
-                } catch (IOException | UnsupportedFlavorException ex) {
-                    throw new RuntimeException(ex);
-                }
-            }
-        });
     }
 
-    public static void processClipboard(Clipboard clipboard) throws IOException, UnsupportedFlavorException {
-        String input = clipboard.getData(DataFlavor.stringFlavor).toString();
-        if (history.size() == 50) {
-            history.remove(49);
+    @Override
+    public void run() {
+        Transferable trans = sysClip.getContents(this);
+        TakeOwnership(trans);
+
+    }
+
+    @Override
+    public void lostOwnership(Clipboard c, Transferable t) {
+
+        try {
+            Main.sleep(250);  //waiting e.g for loading huge elements like word's etc.
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
         }
-        history.add(0,input);
-        for (Rule rule : rules) {
-            if (rule.isDisabled()) {
-                continue;
+        Transferable contents = sysClip.getContents(this);
+        try {
+            processClipboard(contents, c);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+        TakeOwnership(contents);
+
+
+    }
+
+    void TakeOwnership(Transferable t) {
+        sysClip.setContents(t, this);
+    }
+
+    public void processClipboard(Transferable t, Clipboard clipboard) throws IOException, UnsupportedFlavorException {
+        if (!disabled && t != null && t.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+            String input = clipboard.getData(DataFlavor.stringFlavor).toString();
+            if (history.size() == 50) {
+                history.remove(49);
             }
-            Pattern pattern = Pattern.compile(rule.getRegex());
-            Matcher m = pattern.matcher(input);
-            if (m.find()) {
-                int changedIndex = 0;
-                String workingCopy = input;
-                for (int y = 0; y < m.groupCount(); y++) {
-                    workingCopy = workingCopy.substring(0, m.start(y + 1) + changedIndex) + rule.getReplace()[y] + workingCopy.substring(m.end(y + 1) + changedIndex);
-                    int matchLen = m.end(y + 1) - m.start(y + 1);
-                    changedIndex = changedIndex + (rule.getReplace()[y].length() - matchLen);
+            history.add(0, input);
+            for (Rule rule : rules) {
+                if (rule.isDisabled()) {
+                    continue;
                 }
-                StringSelection output = new StringSelection(workingCopy);
-                clipboard.setContents(output, output);
-                break;
+                Pattern pattern = Pattern.compile(rule.getRegex());
+                Matcher m = pattern.matcher(input);
+                if (m.find()) {
+                    int changedIndex = 0;
+                    String workingCopy = input;
+                    for (int y = 0; y < m.groupCount(); y++) {
+                        workingCopy = workingCopy.substring(0, m.start(y + 1) + changedIndex) + rule.getReplace()[y] + workingCopy.substring(m.end(y + 1) + changedIndex);
+                        int matchLen = m.end(y + 1) - m.start(y + 1);
+                        changedIndex = changedIndex + (rule.getReplace()[y].length() - matchLen);
+                    }
+                    StringSelection output = new StringSelection(workingCopy);
+                    clipboard.setContents(output, output);
+                    break;
+                }
             }
         }
     }
@@ -201,12 +200,12 @@ public class Main {
     public static void saveDataFile() throws IOException {
         if (rules.size() > 0) {
             JSONArray jsonArray = new JSONArray();
-            for (int i = 0; i < rules.size(); i++) {
+            for (Rule rule : rules) {
                 JSONObject jsonObject = new JSONObject();
-                jsonObject.put("name", rules.get(i).getName());
-                jsonObject.put("regex", rules.get(i).getRegex());
-                jsonObject.put("replace", rules.get(i).getReplace());
-                jsonObject.put("disabled", rules.get(i).isDisabled());
+                jsonObject.put("name", rule.getName());
+                jsonObject.put("regex", rule.getRegex());
+                jsonObject.put("replace", rule.getReplace());
+                jsonObject.put("disabled", rule.isDisabled());
                 jsonArray.put(jsonObject);
             }
             FileWriter file = new FileWriter(filePath);
